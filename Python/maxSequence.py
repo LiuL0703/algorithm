14426787d1424118fa8dc9f7bd86b6f973b077dc
e <18700790185@163.com>
2017-04-04 15:15:03 +0800

    modifty

--- Python/maxSequence.py
+++ Python/maxSequence.py
@@ -1,14 +1,7 @@
 __author__ = 'liulei'
-
-
 def maxSequence(arr):
-<<<<<<< HEAD
-	this_sum = 0
-	max_sum = 0
-=======
     this_sum = 0
     max_sum = 0
->>>>>>> origin/master
     for i in arr:
         this_sum += i
         if this_sum > max_sum:
@@ -18,4 +11,4 @@ def maxSequence(arr):
     
     return max_sum
 
-maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+# maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
