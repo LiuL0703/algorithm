4e20966d3a926b142a14d8f92c772e601b77aba7
e <18700790185@163.com>
2017-06-08 19:19:31 +0800

    #263

---
+++ LeetCode/Python/#263 Ugly Number.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num <= 0:
+            return False
+        elif num == 1:
+            return True
+
+        while num > 1:
+            if num % 5 == 0:
+                num = num / 5
+            elif num % 3 == 0:
+                num = num / 3
+            elif num % 2 == 0:
+                num = num / 2
+            else:
+                return False
+        return True
