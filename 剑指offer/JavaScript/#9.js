30a33055102aa9c7f38b2a86711cbb2dc07e38e6
liul0703 <louieliu0703@gmail.com>
2018-12-21 15:32:26 +0800

    feat:剑指offer[9]

---
+++ 剑指offer/JavaScript/#9.js
@@ -0,0 +1,23 @@
+
+/**
+ *  一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法
+ *  f(n) = f(0) + f(1) + f(2) + f(3) + ... + f(n-2)+f(n-1)
+    f(n-1) = f(0) + f(1) + f(2) + f(3) + ... + f(n-2)
+    f(n) = 2f(n-1)
+ */
+
+function jumpFloorII(number){
+   if (number === 1) {
+     return number;
+   }
+   return 2*jumpFloorII(number-1);
+}
+
+// 非递归
+function jumpFloorII(number) {
+    let i=1;
+    while(--number){
+        i*=2;
+    }
+    return i;
+}
