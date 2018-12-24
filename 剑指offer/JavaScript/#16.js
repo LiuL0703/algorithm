313f28222c7a7625245e1da90a3e03e15e49794d
liul0703 <louieliu0703@gmail.com>
2018-12-24 11:01:22 +0800

    feat:剑指offer[16]

---
+++ 剑指offer/JavaScript/#16.js
@@ -0,0 +1,31 @@
+/**
+ * 输入两个单调递增的链表，输出两个链表合成后的链表，当然我们需要合成后的链表满足单调不减规则。
+ */
+
+ /*
+ function ListNode(x){
+    this.val = x;
+    this.next = null;
+ }
+*/
+/**
+ * 
+ * @param {LinkList} pHead1 
+ * @param {LinkList} pHead2 
+ */
+function Merge(pHead1, pHead2){
+  let merged = null;
+  if(!pHead1){
+    return pHead2;
+  }
+  if (!pHead2){
+    return pHead1;
+  }
+  if(pHead1.val > pHead2.val){
+    merged = pHead2;
+    merged.next = Merge(pHead1,pHead2.next);
+  }else{
+    merged = pHead1;
+    merged.next = Merge(pHead1.next,pHead2);
+  }
+}
