e659a1ee028bdb7aad5bb3754dff586daf57c31f
liul0703 <louieliu0703@gmail.com>
2018-12-27 16:23:53 +0800

    feat:剑指offer[34]

---
+++ LeetCode/JavaScript/#34.js
@@ -0,0 +1,20 @@
+/**
+ * 在一个字符串(0<=字符串长度<=10000，全部由字母组成)中找到第一个只出现一次的字符,
+ * 并返回它的位置, 如果没有则返回 -1（需要区分大小写）.
+ */
+
+
+/**
+ * 
+ * @param {String} str 
+ */
+function FirstNotRepeatingChar(str){
+  let resStr = str.split('').reverse().join('');
+  let length = str.length;
+  for(let i = 0; i < length; i++){
+    if((str.indexOf(str[i]) + resStr.indexOf(str[i])) === (length - 1)){
+      return i;
+    }
+  }
+  return -1;
+}
