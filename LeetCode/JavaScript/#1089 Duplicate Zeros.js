131afdc6ed33aa32735635c6964f424efc3691ba
Linear-Enter <louieliu0703@gmail.com>
2019-08-03 10:59:07 +0800

    Create #1089 Duplicate Zeros.js
    LeetCode 1089

---
+++ LeetCode/JavaScript/#1089 Duplicate Zeros.js
@@ -0,0 +1,41 @@
+/** 
+ * 
+ Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
+Note that elements beyond the length of the original array are not written.
+Do the above modifications to the input array in place, do not return anything from your function.
+
+ 
+
+Example 1:
+
+Input: [1,0,2,3,0,4,5,0]
+Output: null
+Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+Example 2:
+
+Input: [1,2,3]
+Output: null
+Explanation: After calling your function, the input array is modified to: [1,2,3]
+
+*/
+
+
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+  let lens = arr.length;
+  for(let i = 0; i < lens; i++){
+    if(arr[i] === 0){
+      arr.splice(i,0,0);
+      i++;
+    }
+  }
+  arr.length = lens;
+};
+
+/**
+ * Runtime: 72 ms, faster than 58.46% of JavaScript online submissions for Duplicate Zeros.
+ * Memory Usage: 35.8 MB, less than 100.00% of JavaScript online submissions for Duplicate Zeros.
+ */
