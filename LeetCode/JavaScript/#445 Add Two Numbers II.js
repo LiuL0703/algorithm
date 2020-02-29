7eef3d3aa740b4d8100440693bda6c21a1d94b2a
liul0703 <louieliu0703@gmail.com>
2020-02-29 17:26:21 +0800

    feat(LeetCode):445 Add Two Numbers II

---
+++ LeetCode/JavaScript/#445 Add Two Numbers II.js
@@ -0,0 +1,52 @@
+/**
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The most significant digit comes first and each of their nodes contain a single digit. 
+ * Add the two numbers and return it as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *  Follow up:
+ *    What if you cannot modify the input lists? In other words, reversing the lists is not allowed.
+ *    Example:
+ *      Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+ *      Output: 7 -> 8 -> 0 -> 7
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+  let l1Str = '';
+  let l2Str = '';
+  let res = 0;
+  
+  while(l1){
+      l1Str += l1.val;
+      l1 = l1.next;
+  }
+  while(l2){
+      l2Str += l2.val;
+      l2 = l2.next;
+  }
+  res = ""+ (BigInt(l1Str) + BigInt(l2Str));
+  
+  const stack = res.split('');
+  let head = dumb = new ListNode(null);
+  let lens = stack.length;
+  let count = 0;
+  while(lens--){
+      let tmp = new ListNode(stack[count])
+      dumb.next = tmp;
+      dumb = dumb.next;
+      count++;
+  }
+  
+  return head.next
+};
